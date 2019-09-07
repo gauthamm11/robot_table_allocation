d72cc21e1ca23451d0a17ec3ef6f7cb8c3e21fe5
Gautham Asok <gauthamasok.dev@gmail.com>
2019-09-07 22:16:34 +0530

    basic functionality

--- process.php
+++ process.php
@@ -1,10 +1,18 @@
 <?php
 if (!isset($_GET['min']) && !isset($_GET['max'])) {
-    header("location: index.php");
+	header("location: index.php");
 }else{
-    $min = $_GET['min'];
+	$min = $_GET['min'];
     $max = $_GET['max'];
     
+	$db = new SQLite3('robot.db');
+
+	$results = $db->query('SELECT * FROM `customer_details` WHERE c_count BETWEEN '.$min.' and '.$max.' ORDER BY entry_time ASC;');
+	while ($row = $results->fetchArray()) {
+    //var_dump($row);
+		echo $row['c_name']."<br>";
+	}
+
 }
 
 ?>
