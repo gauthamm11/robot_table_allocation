12cb143fe55447964c876d0d4a17cc427f9e0289
Gautham Asok <gauthamasok.dev@gmail.com>
2019-09-07 23:59:54 +0530

    updated code

--- action.php
+++ action.php
@@ -14,12 +14,22 @@ if (!isset($_GET['sno']) && !isset($_GET['action'])) {
         Customer is snoozed for sometime!
         </div>
         ';
-    }else{
+    }elseif($action == 1){
         // accepted
+        $dbq = new SQLite3('robot.db');
 
-
-    }
-
+    $resultsq = $dbq->query('DELETE FROM customer_details WHERE sno = '.$sno.';');
+    
+    echo '
+    <div class="alert alert-success" role="alert" id="snzalert">
+    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+    Customer is Alloted Successfully!
+    </div>
+    ';
+   
+}else{
+    echo "qwerty";
+}
 
 }
 ?>
